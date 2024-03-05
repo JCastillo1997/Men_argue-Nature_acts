ea6b293738d8165cec7f932d335eeb53bf5e8bc7
Jaime Castillo <Castillof1997@gmail.com>
2024-03-05 16:59:35 +0100

    Created functions, realized scraping might be not so much as a good idea

--- FUNCTIONS/functions.py
+++ FUNCTIONS/functions.py
@@ -59,3 +59,33 @@ def air_url(search_query):
 search_query = input("Please enter the search query: ")
 url = air_url(search_query)
 print("The URL of the page is:", url)
+
+
+def get_air_quality_data(city):
+    import requests
+    import pandas as pd
+
+    api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+    response = requests.get(api_url, headers={'X-Api-Key': 'XhKog+U7kqeVm1jW7MeNDg==ZsJmH2sbNbEnRq3H'})
+    if response.status_code == requests.codes.ok:
+        air_quality_data = response.json()
+        if 'error' in air_quality_data:
+            raise ValueError("Error: City not found in the database")
+        else:
+            # Convert the JSON data to a dataframe
+            data = {k.lower(): [v['concentration'], v['aqi']] for k, v in air_quality_data.items() if k != 'overall_aqi'}
+            data['overall_aqi'] = [None, air_quality_data['overall_aqi']]
+            pollution_df = pd.DataFrame(data)
+            return pollution_df
+    else:
+        raise requests.ConnectionError(f"Error: {response.status_code} - {response.text}")
+
+# Test the function with a city
+city_name = input("Enter a city name: ")  # Prompt the user to enter a city name
+try:
+    pollution_df = get_air_quality_data(city_name)
+    print(pollution_df)
+except ValueError as ve:
+    print(ve)
+except requests.ConnectionError as ce:
+    print(ce)
